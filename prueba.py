84db33d5a93d8784abdd152663f1e323b136fc54
a01658416-ship-it <a01658416@tec.mx>
2025-11-27 15:41:44 -0600

    Predicciones Homicidio y abuso

--- prueba.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-# Carga el archivo problemático
-contenido = joblib.load("forecast_col_negocios.pickle")
-
-# Imprime qué es realmente
-print("Tipo de dato:", type(contenido))
-print("Contenido:", contenido)
